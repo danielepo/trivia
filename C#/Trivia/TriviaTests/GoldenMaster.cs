3d29248865dde0dcf405d11f93b234c6fac7cd49
danielepo <dnl.pozzobon@gmail.con>
2016-07-21 10:37:35 +0200

    Creates GoldMaster

--- C#/Trivia/TriviaTests/GoldenMaster.cs
+++ C#/Trivia/TriviaTests/GoldenMaster.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 using NUnit.Framework;
 using Trivia;
 namespace TriviaTests
@@ -11,14 +13,16 @@ namespace TriviaTests
         public void TestMethod1()
         {
             // let's try to log all the output from a sigle game
-            
+
             var file = File.CreateText("gold.txt");
 
             Console.SetOut(file);
-            string[] args = new string[1];
+            string[] args = new string[2];
+            args[0] = "true";
+            var rand = new Random();
             for (int i = 0; i < 100; i++)
             {
-                args[0] = i.ToString();
+                args[1] = i.ToString();//rand.Next().ToString();
                 GameRunner.Main(args);
             }
 
@@ -26,5 +30,81 @@ namespace TriviaTests
             file.Close();
         }
 
+        [Test]
+        public void TestWithGold()
+        {
+            var file = File.ReadLines("gold.txt");
+            bool parseSeed = false;
+            MemoryStream stream;
+            StreamWriter writer;
+            StreamReader reader;
+            stream = new MemoryStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+            Console.SetOut(writer);
+            string currentSeed = String.Empty;
+            foreach (var line in file)
+            {
+
+                switch (line)
+                {
+                    case "**********":
+                        parseSeed = true;
+                        continue;
+                    case "----------":
+                        continue;
+                    case "__________":
+                        stream.SetLength(0);
+                        continue;
+                }
+
+                if (parseSeed)
+                {
+                    currentSeed = line;
+                    parseSeed = false;
+                    GameRunner.Main(new[] { "false", currentSeed });
+                    writer.Flush();
+                    stream.Position = 0;
+                    continue;
+                }
+
+
+                Assert.AreEqual(line, reader.ReadLine(), "Seed " + currentSeed);
+            }
+        }
+
+        [Test]
+        public void testRandomSeed()
+        {
+            var seed1 = 1079290332;
+            var seed2 = 2051736430;
+            var rand = new Random(seed1);
+            List<int> mem1 = new List<int>();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                mem1.Add(rand.Next());
+            }
+            rand = new Random(seed2);
+            List<int> mem2 = new List<int>();
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                mem2.Add(rand.Next());
+            }
+            rand = new Random(seed1);
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Assert.AreEqual(mem1[i], rand.Next());
+            }
+            rand = new Random(seed2);
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                Assert.AreEqual(mem2[i], rand.Next());
+            }
+        }
     }
+
 }
